c5e4d4838cd086ff6b26364bf69a26c5d380d153
ghlee <94736572+neo622@users.noreply.github.com>
2022-08-24 23:43:02 +0900

    Create [Programmers] 최소 직사각형.py

---
+++ 문제 풀이/[Programmers] 최소 직사각형.py
@@ -0,0 +1,19 @@
+# level 1
+# sizes = [[60, 50], [30, 70], [60, 30], [80, 40]]
+# https://school.programmers.co.kr/learn/courses/30/lessons/86491
+
+def solution(sizes):
+    answer = 0
+    w = []
+    h = []
+    for i in range(len(sizes)):
+        if sizes[i][0] >= sizes[i][1]:
+            w.append(sizes[i][0])
+            h.append(sizes[i][1])
+        else:
+            w.append(sizes[i][1])
+            h.append(sizes[i][0])
+    answer += max(w) * max(h)
+    
+    return answer
+        
